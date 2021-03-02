57c2c5c5e83c0e27375fbc420ce609dac825154a
arosicki <justnobody856@gmail.com>
2021-03-02 13:01:40 +0100

    add settings

---
+++ settings/settings.php
@@ -0,0 +1,7 @@
+<?php
+$_ENV['DB_HOST'] = '';
+$_ENV['DB_NAME'] = '';
+$_ENV['DB_USERNAME'] = '';
+$_ENV['DB_PASSWORD'] = '';
+$_ENV['DB_PORT'] = 3306;
+$_ENV['TOKEN_TTL'] = 3600; //in seconds (recommended minimum value: 1200)
